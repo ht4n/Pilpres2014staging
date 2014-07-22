458c00ae88808266c4c2b545cd20e284f8a72627
Henry Tan <htansetiawan@gmail.com>
2014-07-22 00:12:39 -0700

    deploy.cmd

--- deploy.cmd
+++ deploy.cmd
@@ -1,16 +1,26 @@
 copy %1\*.html
 
+
 copy %1\*.js
+
 copy %1\*.js.map
+
 copy %1\*.css
+
 
 copy %1\*.json
 
+
 copy %1\*.config
 
+
 robocopy %1\Images Images /E
+
 robocopy %1\Resources Resources /E
 
+
 git add *
+
 git commit -m "deploy"
+
 git push
